16aaa847fac95cb2b1791b8e127e07c059630ebd
PiotrGliszczynski <piotr.gliszczynski@gmail.com>
2018-04-24 19:37:39 +0200

    Files added via upload
    version V1.0
    - changing one or three values in proportional way in number of steps to reach the required value

---
+++ Ramp.h
@@ -0,0 +1,22 @@
+class Ramp
+{
+  public:
+    Ramp( boolean rgb );
+    boolean at_sp();
+    void set_sp( int color_r, int color_g, int color_b, int steps);
+    void set_sp( int color_w, int change_time);
+    void do_step();
+    int get_val( byte index ); 
+    int get_sp( byte index ); 
+    byte get_steps();
+  private:
+    void _calc_steps( int steps );
+    boolean _at_sp;
+    byte _ch_num;
+    float _step[3];
+    float _sp[3];
+    float _value[3];
+    int _value_hw[3];
+    byte _steps;
+};
+
